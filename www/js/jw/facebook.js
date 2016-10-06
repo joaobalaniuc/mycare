c64f19d00946ec05e21dfcd84891751947ca45ac
João Williams Balaniúc <jwillbala@hotmail.com>
2016-10-06 16:52:31 -0300

    master

--- www/js/jw/facebook.js
+++ www/js/jw/facebook.js
@@ -4,18 +4,29 @@
 
 var fb = {
     login: function () {
-        facebookConnectPlugin.login(["email", "public_profile"], function (result) {
+        facebookConnectPlugin.login(['email', 'public_profile', 'user_birthday'], function (result) {
             alert("fb.login() = " + JSON.stringify(result));
             localStorage.fb_id = result.authResponse.userID;
             localStorage.fb_token = result.authResponse.accessToken;
             localStorage.fb_status = 'connected';
+
+            //
+            facebookConnectPlugin.api("/me?fields=id,birthday,gender,first_name,age_range,last_name,name,picture.width(400),email", [],
+                    function (result) {
+                        alert("/me = " + JSON.stringify(result));
+                    },
+                    function (error) {
+                        alert("/me failed = " + error);
+                    });
+            //
+
         }, function (err) {
             alert('an error occured while trying to login. please try again. Err:' + err);
         });
     },
     getUserInfo: function () {
         //facebookConnectPlugin.api(localStorage.fb_id + "/?fields=id,email,first_name,last_name,gender,picture,birthday", ["public_profile", "user_birthday"],
-        facebookConnectPlugin.api("/me/", ["public_profile"],
+        facebookConnectPlugin.api("/me", ["public_profile"],
                 function (result) {
                     alert("fb.getUserInfo() = " + JSON.stringify(result));
                     localStorage.fb_id = result.id;
@@ -35,8 +46,10 @@ var fb = {
         facebookConnectPlugin.getLoginStatus(
                 function (response) {
 
+                    alert("fb.getLoginStatusX() = " + JSON.stringify(response));
+
                     localStorage.fb_status = response.status;
-                    
+
                     if (response.status === 'connected') {
                         var uid = response.authResponse.userID;
                         var accessToken = response.authResponse.accessToken;
