fcaad9ebc6f646bac26f81e73ab097fa3ccb240d
João Williams Balaniúc <jwillbala@hotmail.com>
2016-10-11 12:33:30 -0300

    master

--- www/js/jw/page.post_my.js
+++ www/js/jw/page.post_my.js
@@ -28,50 +28,54 @@ $.ajax({
                     $("#nenhum").show();
                     return;
                 }
+                if (res[0]) {
+                    $.each(res, function (key, val) {
 
-                $.each(res, function (key, val) {
+                        $("#post_template")
+                                .clone()
+                                .prop({
+                                    id: "post_" + val["post_id"]
+                                })
+                                .appendTo("#ads")
+                                .attr("data-id", val["post_id"])
+                                .attr("data-title", val["post_title"])
+                                .attr("data-com", val["post_com"]) // permite comentarios?
+                                .attr("data-cnpj", val["post_cnpj"]);
+                        ;
 
-                    $("#post_template")
-                            .clone()
-                            .prop({
-                                id: "post_" + val["post_id"]
-                            })
-                            .appendTo("#ads")
-                            .attr("data-id", val["post_id"])
-                            .attr("data-title", val["post_title"])
-                            .attr("data-com", val["post_com"]) // permite comentarios?
-                            .attr("data-cnpj", val["post_cnpj"]);
-                    ;
+                        $("#post_" + val["post_id"]).each(function (index) {
 
-                    $("#post_" + val["post_id"]).each(function (index) {
+                            if (val["post_status"] == "1") {
+                                var post_status = "<span class='light-green-text'><img src='img/ico-ok.png' style='width:16px;display:inline-block;vertical-align:text-bottom' /> ATIVO</span>";
+                            }
+                            if (val["post_status"] == "2") {
+                                var post_status = "<span class='grey-text'><img src='img/ico-wait.png' style='width:16px;display:inline-block;vertical-align:text-bottom' /> EM ANÁLISE</span>";
+                            }
+                            if (val["post_status"] == "0") {
+                                var post_status = "<span class='red-text'><img src='img/ico-block.gif' style='width:16px;display:inline-block;vertical-align:text-bottom' /> BLOQUEADO</span>";
+                            }
+                            $(this).find(".post_title").html(val["post_title"]);
+                            $(this).find(".post_txt").html(val["post_txt"]);
+                            $(this).find(".post_date").html(val["post_date"]);
+                            $(this).find(".post_status").html(post_status + " <br/><span style='color:#ccc'>#" + val["post_id"] + "</span>");
 
-                        if (val["post_status"] == "1") {
-                            var post_status = "<span class='light-green-text'><img src='img/ico-ok.png' style='width:16px;display:inline-block;vertical-align:text-bottom' /> ATIVO</span>";
+                            var rand = getRandomInt(1, 7);
+                            $(this).find(".post_img").attr("src", "img/simula" + rand + ".jpg");
+                        }).show();
+
+                    });
+
+                    $(".post_txt").text(function (index, currentText) {
+                        if (currentText.length > 40) {
+                            return currentText.substr(0, 40) + " ...";
                         }
-                        if (val["post_status"] == "2") {
-                            var post_status = "<span class='grey-text'><img src='img/ico-wait.png' style='width:16px;display:inline-block;vertical-align:text-bottom' /> EM ANÁLISE</span>";
-                        }
-                        if (val["post_status"] == "0") {
-                            var post_status = "<span class='red-text'><img src='img/ico-block.gif' style='width:16px;display:inline-block;vertical-align:text-bottom' /> BLOQUEADO</span>";
-                        }
-                        $(this).find(".post_title").html(val["post_title"]);
-                        $(this).find(".post_txt").html(val["post_txt"]);
-                        $(this).find(".post_date").html(val["post_date"]);
-                        $(this).find(".post_status").html(post_status + " <br/><span style='color:#ccc'>#" + val["post_id"] + "</span>");
+                    });
 
-                        var rand = getRandomInt(1, 7);
-                        $(this).find(".post_img").attr("src", "img/simula" + rand + ".jpg");
-                    }).show();
+                    pretty();
 
-                });
-
-                $(".post_txt").text(function (index, currentText) {
-                    if (currentText.length > 40) {
-                        return currentText.substr(0, 40) + " ...";
-                    }
-                });
-
-                pretty();
-
+                } // res[0]
+                else {
+                    $("#nenhum").show();
+                }
             } // res not null
         }); // after ajax
